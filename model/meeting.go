a5e7fb50317796bbdc7b4f1ebb6f6f5ab06de4ec
iixin <s6777v@gmail.com>
2023-03-31 22:03:18 +0800

    update meeting.go

--- model/meeting.go
+++ model/meeting.go
@@ -1,11 +1,22 @@
 package model
 
 type Meeting struct {
-	ChatId            string
-	SpaceId           string
-	RecordIdInBitable string
-	Topic             string
-	MeetingId         string
-	StartTime         string
-	EndTime           string
+	ID                uint   `gorm:"not null;autoIncrement;primaryKey"`
+	CalendarID        string `gorm:"not null;size:65"`
+	GroupChatID       string `gorm:"not null;uniqueIndex;size:45"`
+	EventId           string `gorm:"not null;size:45"`
+	RecordIdInBitable string `gorm:"not null;size:45"`
+	Topic             string `gorm:"not null;size:255"`
+	StartTime         uint64 `gorm:"not null"`
+	EndTime           uint64 `gorm:"not null"`
+}
+
+func QueryMeetingById(id uint) (*Meeting, error) {
+	var result Meeting
+	err := gormDb.First(&result, id)
+	return &result, err.Error
+}
+func CreateMeeting(meeting *Meeting) (uint, error) {
+	err := gormDb.Create(meeting)
+	return meeting.ID, err.Error
 }
