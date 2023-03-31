95b6f941091b30776083736d40a00dbf9ca1b02c
iixin <s6777v@gmail.com>
2023-03-31 17:05:14 +0800

    add meeting.go

---
+++ model/meeting.go
@@ -0,0 +1,11 @@
+package model
+
+type Meeting struct {
+	ChatId            string
+	SpaceId           string
+	RecordIdInBitable string
+	Topic             string
+	MeetingId         string
+	StartTime         string
+	EndTime           string
+}
